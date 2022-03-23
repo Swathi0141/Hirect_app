91aabfd0707641bcb5295a2e44e404e5fa9e19b0
Swathi0141 <sv222@njit.edu>
2022-03-23 19:16:17 -0400

    modified download page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Downloadpage from './components/downloadpage';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Downloadpage/>,
   document.getElementById('root')
 );
 
